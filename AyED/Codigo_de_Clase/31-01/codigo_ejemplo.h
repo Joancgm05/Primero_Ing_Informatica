d2c01b0a2d3526cc6a93a5e74f7c729c5820bd08
Joancgm05 <alu010165571@ull.edu.es>
2024-02-04 22:25:40 +0000

    mc

--- AyED/Codigo_de_Clase/31-01/codigo_ejemplo.h
+++ AyED/Codigo_de_Clase/31-01/codigo_ejemplo.h
@@ -8,20 +8,20 @@ typedef std::vector<double> points_container; // Crea un alias para un vector
 class func_eval {
  public:
   func_eval(const double left, const double right, const double eps, const function_ptr function) {
-    assert(right >= left +eps);
+    assert(right >= left + eps);
     fill_x(x_, left, right, eps);
   }
  private:
    points_container x_;
    points_container y_;
-   void fill_x(points_container & x, const double left, const double right, const double eps) {
+   void fill_x(points_container &x, const double left, const double right, const double eps) {
      double point (left);
      while (point <= right) {
        x.emplace_back(point);
        point += eps;
      }
    }
-   void eval(const points_container & x , points_container & y, function_ptr function) {
+   void eval(const points_container &x, points_container &y, function_ptr function) {
      for (const double point : x) {
        y.emplace_back(function(point));
      }
