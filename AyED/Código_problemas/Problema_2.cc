b0426b8bc76a3649f17f8d6946f9481b2f4e1fef
Joancgm05 <alu010165571@ull.edu.es>
2024-02-06 19:07:42 +0000

    mc

--- AyED/Código_problemas/Problema_2.cc
+++ AyED/Código_problemas/Problema_2.cc
@@ -6,13 +6,20 @@ int main() {
   int *ptri, *ptrf, aux;
   ptri = vector; // Este puntero apunta al comienzo del vector
   ptrf = vector + 6; // Este puntero apunta al final del vector
-
   while (ptri < ptrf) {
     aux = *ptri; // Guardo el vector del inicio en aux
     *ptri = *ptrf; // Intercambio los valores del puntero del inicio y el del fin
     *ptrf = aux;
     ++ptri; // Aumento la posición a la que apunta
     --ptrf; // Decremento la posición a la que apunta
+  }
+  
+  for (int i{0}; i < 7; ++i) {
+    if (i == 6) {
+      std::cout << vector[i] << ".";
+    } else {
+        std::cout << vector[i] << ", ";
+      }
   }
   return 0;
 }
