2c0ba84bc55a71aa96a9c1ba46b608ecdb97373f
Joancgm05 <alu0101656571@ull.edu.es>
2024-03-01 00:32:03 +0000

    Comentarios del main P02 AyED

--- AyED/Practicas/Practica_2/main_p2.cpp
+++ AyED/Practicas/Practica_2/main_p2.cpp
@@ -25,44 +25,44 @@ using namespace std;
 
 int main()
 {
-  rational_t a(1, 2), b(3);
+  rational_t a(1, 2), b(3); // Se crea un racional con numerador 1 y denominador 2 y otro con numerador 3 y denominador 1
 
   // FASE I
-  cout << "a + b: ";
-  (a+b).write();
+  cout << "a + b: "; // Se muestra por pantalla la suma de los racionales
+  (a + b).write();
 
-  cout << "b - a: ";
-  (b-a).write();
+  cout << "b - a: "; // Se muestra por pantalla la resta de los racionales
+  (b - a).write();
 
-  cout << "a * b: ";
-  (a*b).write();
+  cout << "a * b: "; // Se muestra por pantalla el producto de los racionales
+  (a * b).write();
   
-  cout << "a / b: ";
-  (a/b).write();
+  cout << "a / b: "; // Se muestra por pantalla la división de los racionales 
+  (a / b).write();
   
   cout << endl;
   
   // FASE II
-  vector_t<double> v, w;
-  v.read(), v.write();
-  w.read(), w.write();
+  vector_t<double> v, w; // Se crean dos vectores de tipo double
+  v.read(), v.write(); // Se leen y muestran por pantalla los vectores
+  w.read(), w.write(); // Se leen y muestran por pantalla los vectores
   
-  cout << "Producto escalar de vector_t<double>: " << scal_prod(v, w) << endl << endl;
+  cout << "Producto escalar de vector_t<double>: " << scal_prod(v, w) << endl << endl; // Se muestra por pantalla el producto escalar de los vectores
   
-  vector_t<rational_t> x, y;
+  vector_t<rational_t> x, y; // Se crean dos vectores de tipo rational_t
   x.read(), x.write();
   y.read(), y.write();
   
-  cout << "Producto escalar de vector_t<rational_t>: " << scal_prod(x, y) << endl << endl;
+  cout << "Producto escalar de vector_t<rational_t>: " << scal_prod(x, y) << endl << endl; // Se muestra por pantalla el producto escalar de los vectores
   
   
   // FASE III
-  matrix_t<double> A, B, C;
-  A.read(), A.write();
-  B.read(), B.write();
+  matrix_t<double> A, B, C; // Se crean matrices de tipo double
+  A.read(), A.write(); // Se leen y muestran por pantalla las matrices
+  B.read(), B.write(); // Se leen y muestran por pantalla las matrices
   
-  C.multiply(A, B);
-  cout << "Multiplicación de matrices A y B: " << endl;
+  C.multiply(A, B); // Se multiplican las matrices A y B, se almacena en C
+  cout << "Multiplicación de matrices A y B: " << endl; // Se muestra por pantalla la multiplicación de las matrices
   C.write();
   
   return 0;
