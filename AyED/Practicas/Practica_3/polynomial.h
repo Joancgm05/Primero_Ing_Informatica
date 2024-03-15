e73db5b6972294d6d1dc2d61bf5186859bf12ba1
Joancgm05 <alu0101656571@ull.edu.es>
2024-03-15 00:34:01 +0000

    Refactor polynomial classes and add comments

--- AyED/Practicas/Practica_3/polynomial.h
+++ AyED/Practicas/Practica_3/polynomial.h
@@ -18,22 +18,21 @@
 #include "sparse_vector_t.h"
 
 // Clase para polinomios basados en vectores densos de doubles
-class Polynomial : public vector_t<double> {
+class Polynomial : public vector_t<double> { // hereda de vector_t
  public:
   // constructores
-  Polynomial(const int n = 0) : vector_t<double>(n) {};
-  Polynomial(const Polynomial& pol)
-      : vector_t<double>(pol) {}; // constructor de copia
+  Polynomial(const int n = 0) : vector_t<double>(n) {}; // constructor normal
+  Polynomial(const Polynomial& pol) : vector_t<double>(pol) {}; // constructor de copia
 
   // destructor
   ~Polynomial() {};
 
   // E/S
-  void Write(std::ostream& = std::cout, const double eps = EPS) const;
+  void Write(std::ostream& = std::cout, const double eps = EPS) const; // escribe el polinomio en un flujo de salida
   
   // operaciones
-  double Eval(const double) const;
-  bool IsEqual(const Polynomial&, const double = EPS) const;
+  double Eval(const double) const; // evalúa el polinomio en un punto
+  bool IsEqual(const Polynomial&, const double = EPS) const; // compara si dos polinomios son iguales
  };
 
 
@@ -41,40 +40,40 @@ class Polynomial : public vector_t<double> {
 class SparsePolynomial : public sparse_vector_t {
  public:
   // constructores
-  SparsePolynomial(const int n = 0) : sparse_vector_t(n) {};
-  SparsePolynomial(const Polynomial& pol) : sparse_vector_t(pol) {};
+  SparsePolynomial(const int n = 0) : sparse_vector_t(n) {}; // constructor normal
+  SparsePolynomial(const Polynomial& pol) : sparse_vector_t(pol) {}; // constructor de conversión
   SparsePolynomial(const SparsePolynomial&);  // constructor de copia
 
   // destructor
   ~SparsePolynomial() {};
 
   // E/S
-  void Write(std::ostream& = std::cout) const;
+  void Write(std::ostream& = std::cout) const; // escribe el polinomio en un flujo de salida
   
   // operaciones
-  double Eval(const double) const;
-  bool IsEqual(const SparsePolynomial&, const double = EPS) const;
-  bool IsEqual(const Polynomial&, const double = EPS) const;
+  double Eval(const double) const; // evalúa el polinomio en un punto
+  bool IsEqual(const SparsePolynomial&, const double = EPS) const; // compara si dos polinomios son iguales
+  bool IsEqual(const Polynomial&, const double = EPS) const; // compara si dos polinomios son iguales
 };
 
 // E/S
-void Polynomial::Write(std::ostream& os, const double eps) const {
-  os << get_size() << ": [ ";
-  bool first{true};
-  for (int i{0}; i < get_size(); i++)
-    if (IsNotZero(at(i), eps)) {
-      os << (!first ? " + " : "") << at(i)
-	 << (i > 1 ? " x^" : (i == 1) ? " x" : "");
-      if (i > 1)
-	os << i;
-      first = false;
-    }
-  os << " ]" << std::endl;
+void Polynomial::Write(std::ostream& os, const double eps) const { // escribe el polinomio en un flujo de salida
+  os << get_size() << ": [ "; // escribe el tamaño del polinomio
+  bool first{true}; // booleano para saber si es el primer elemento
+  for (int i{0}; i < get_size(); i++) // recorre el polinomio
+    if (IsNotZero(at(i), eps)) { // si el valor es distinto de cero
+      os << (!first ? " + " : "") << at(i) // escribe el valor
+	 << (i > 1 ? " x^" : (i == 1) ? " x" : ""); // escribe el índice
+      if (i > 1) // si el índice es mayor que 1
+	os << i; // escribe el índice
+      first = false; // ya no es el primer elemento
+    } 
+  os << " ]" << std::endl; // escribe el final del polinomio
 }
 
-std::ostream& operator<<(std::ostream& os, const Polynomial& p) {
-  p.Write(os);
-  return os;
+std::ostream& operator<<(std::ostream& os, const Polynomial& p) { // sobrecarga del operador de salida
+  p.Write(os); // escribe el polinomio en un flujo de salida
+  return os; // devuelve el flujo de salida
 }
 
 // Operaciones con polinomios
@@ -83,6 +82,8 @@ std::ostream& operator<<(std::ostream& os, const Polynomial& p) {
 double Polynomial::Eval(const double x) const {
   double result{0.0};
   // poner el código aquí
+  for (int i{0}; i < get_size(); i++) // recorre el polinomio
+    result += at(i) * pow(x, i); // evalúa el polinomio en un punto (x^i * at(i)
   return result;
 }
 
@@ -90,7 +91,13 @@ double Polynomial::Eval(const double x) const {
 bool Polynomial::IsEqual(const Polynomial& pol, const double eps) const {
   bool differents = false;
   // poner el código aquí
+  if (get_size() != pol.get_size()) {// si los tamaños son distintos
+    differents = true; // son distintos
+  } else {
+    for (int i{0}; i < get_size(); i++) // recorre el polinomio
+      if (fabs(at(i) - pol.at(i)) > eps) // si los valores son distintos
   return !differents;
+  }
 }
 
 // constructor de copia
@@ -99,23 +106,23 @@ SparsePolynomial::SparsePolynomial(const SparsePolynomial& spol) {
 }
 
 // E/S
-void SparsePolynomial::Write(std::ostream& os) const {
-  os << get_n() << "(" << get_nz() << "): [ ";
-  bool first{true};
-  for (int i{0}; i < get_nz(); i++) {
-    int inx{at(i).get_inx()};
-    os << (!first ? " + " : "") << at(i).get_val()
-       << (inx > 1 ? " x^" : (inx == 1) ? " x" : "");
-    if (inx > 1)
-      os << inx;
-    first = false;
+void SparsePolynomial::Write(std::ostream& os) const { // escribe el polinomio en un flujo de salida
+  os << get_n() << "(" << get_nz() << "): [ "; // escribe el tamaño del polinomio y el nº de valores distintos de cero
+  bool first{true}; // booleano para saber si es el primer elemento
+  for (int i{0}; i < get_nz(); i++) { // recorre el polinomio
+    int inx{at(i).get_inx()}; // obtiene el índice
+    os << (!first ? " + " : "") << at(i).get_val() // escribe el valor
+       << (inx > 1 ? " x^" : (inx == 1) ? " x" : ""); // escribe el índice
+    if (inx > 1) // si el índice es mayor que 1
+      os << inx; // escribe el índice
+    first = false; // ya no es el primer elemento
   }
-  os << " ]" << std::endl;
+  os << " ]" << std::endl; // escribe el final del polinomio
 }
 
 std::ostream& operator<<(std::ostream& os, const SparsePolynomial& p) {
-  p.Write(os);
-  return os;
+  p.Write(os); // escribe el polinomio en un flujo de salida
+  return os; // devuelve el flujo de salida
 }
 
 // Operaciones con polinomios
@@ -124,14 +131,21 @@ std::ostream& operator<<(std::ostream& os, const SparsePolynomial& p) {
 double SparsePolynomial::Eval(const double x) const {
   double result{0.0};
   // poner el código aquí
+  for (int i{0}; i < get_nz(); i++) // recorre el polinomio
+    result += at(i).get_val() * pow(x, at(i).get_inx()); // evalúa el polinomio en un punto (x^i * at(i)
   return result;
 }
 
 // Comparación si son iguales dos polinomios representados por vectores dispersos
-bool SparsePolynomial::IsEqual(const SparsePolynomial& spol
-			       , const double eps) const {
+bool SparsePolynomial::IsEqual(const SparsePolynomial& spol, const double eps) const { 
   bool differents = false;
   // poner el código aquí
+  int Tam_vec = 0;
+  if (get_nz() != spol.get_nz()) { return differents; } // Comprobar si el tamaño de los vectores es igual
+  for (int i{0}; i < get_nz(); i++){ // recorre el polinomio
+    if (at(i).get_inx() != spol.at(i).get_inx()){ differents = true; } // Comprobar si el valor de los índices son iguales
+    if (fabs(at(i).get_val() - spol.at(i).get_val()) >= eps){ differents = true; } // Comprobar si los valores son iguales
+  }
   return !differents;
 }
 
@@ -140,6 +154,11 @@ bool SparsePolynomial::IsEqual(const SparsePolynomial& spol
 bool SparsePolynomial::IsEqual(const Polynomial& pol, const double eps) const {
   bool differents = false;
   // poner el código aquí
+  if (get_n() != pol.get_size()) { return differents; } // Comprobar si el tamaño de los vectores es igual
+  for (int i{0}; i < get_nz(); i++){ // recorre el polinomio
+    if (at(i).get_inx() != i){ differents = true; } // Comprobar si el valor de los índices son iguales
+    if (fabs(at(i).get_val() - pol.at(i)) >= eps){ differents = true; } // Comprobar si los valores son iguales
+  }
   return !differents;
 }
 
