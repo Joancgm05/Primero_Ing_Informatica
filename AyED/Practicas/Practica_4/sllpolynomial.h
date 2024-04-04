76578bb056b2c372b13f791e3cf66b1305c9e368
Joancgm05 <alu0101656571@ull.edu.es>
2024-04-04 19:58:06 +0100

    mc

--- AyED/Practicas/Practica_4/sllpolynomial.h
+++ AyED/Practicas/Practica_4/sllpolynomial.h
@@ -1,6 +1,6 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
+// AUTOR: Joan Carlos Garcia Méndez 
+// FECHA: 01/04/2024
+// EMAIL: alu0101656571@ull.edu.es
 // VERSION: 2.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 4
