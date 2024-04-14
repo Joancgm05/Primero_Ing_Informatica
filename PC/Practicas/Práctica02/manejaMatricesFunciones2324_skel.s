f3d6584a0da6ebcfcf67c5b38075229a970f2d85
Joancgm05 <alu0101656571@ull.edu.es>
2024-04-14 15:01:36 +0100

    mc

--- PC/Practicas/Práctica02/manejaMatricesFunciones2324_skel.s
+++ PC/Practicas/Práctica02/manejaMatricesFunciones2324_skel.s
@@ -271,38 +271,443 @@ swap_fin:
 intercambia:
 
 	la 		$t0, 8($a1) # Se carga en $t0 el primer elemento de la matriz
-	lw		$t1, 4($a1) # Se carga en $t1 el número de columnas
-	lw 		$t2, 0($a1) # Se carga en $t2 el número de filas
+	lw		$t1, 0($a1) # Se carga en $t1 el número de filas
+	lw 		$t2, 4($a1) # Se carga en $t2 el número de columnas
 	#float* e1 = &datos[indF * numCol + indC];
-	mul 	$t5, $a2, $t1 # [indF * numCol
+	mul 	$t5, $a2, $t2 # [indF * numCol
 	addu 	$t5, $t5, $a3 # [indF * numCol + indC]
 	mul 	$t5, $t5, sizeF # Se le multiplica el tamaño del flotante (4)
 	addu 	$t3, $t0, $t5 # Almacena la nueva dirección
 	move 	$a0, $t3
 	
 	#float* e2 = &datos[(numFil - indF - 1) * numCol + (numCol - indC - 1)];
-	sub $t8, $t1, $a2 # numFil - indF
-	li $t7, 1
-	sub $t8, $t8, $t7 # [(numFil - indF - 1)
-	mul $t8, $t8, $t2 # [(numFil - indF - 1) * numCol
-	sub $t4, $t2, $a3 # (numCol - indC
-	sub $t4, $t4, $t7 # (numCol - indC - 1)]
-	addu $t8, $t8, $t4 # [(numFil - indF - 1) * numCol + (numCol - indC - 1)]
-	mul $t8, $t8, sizeF # Se multiplica por el tamaño del flotante (4)
-	addu $t8, $t8, $t0 # Se almacena la dirección
-	move $a3, $t8
+	sub 	$t8, $t1, $a2 # numFil - indF
+	li 		$t7, 1
+	sub 	$t8, $t8, $t7 # [(numFil - indF - 1)
+	mul 	$t8, $t8, $t2 # [(numFil - indF - 1) * numCol
+	sub		$t4, $t2, $a3 # (numCol - indC
+	sub 	$t4, $t4, $t7 # (numCol - indC - 1)]
+	addu 	$t8, $t8, $t4 # [(numFil - indF - 1) * numCol + (numCol - indC - 1)]
+	mul 	$t8, $t8, sizeF # Se multiplica por el tamaño del flotante (4)
+	addu 	$t8, $t8, $t0 # Se almacena la dirección
+	move 	$a3, $t8
 	
 	# Pila para almacenar la dirección
-	addi $sp, $sp, -4 
-	sw $ra, 0($sp)
+	addi 	$sp, $sp, -4 
+	sw 		$ra, 0($sp)
 
 	# Vuelvo a llamar a la función
-	jal swap 
+	jal 	swap 
 
 	# Recupero la dirección almacenada en la pila
+	lw 		$ra, 0($sp)
+	addi 	$sp, $sp, 4
+
+	jr 		$ra
+
+intercambia_fin:
+
+# subrutina FIND-MIN
+
+find_min:
+	la 		$t0, 8($a1) # Se carga en $t0 el primer elemento de la matriz
+	lw 		$t1, 0($a1) # Se carga en $t1 el número de filas
+	lw 		$t2, 4($a1) # Se carga en $t2 el número de columnas
+	
+	move 	$t4, $zero # Cargo el índice de filas a 0
+	
+	l.s 	$f4, infinito #float min = infinito;
+	li 		$t6, -1 # int fmin = -1;
+	li 		$t7, -1 # int cmin = -1;
+
+	mul 	$t8, $t1, $t2 # Se obtiene el tamaño de la matriz
+	beqz 	$t8, find_out
+
+# for(int f = 0; f < nFil; f++) {
+for_find_min_one: 
+	move 	$t5, $zero # inicializo el índice de columnas a 0
+	mul		 $t8, $t4, $t2 #$t8 = i*ncol
+# for(int c = 0; c < numCol; c++) {
+for_find_min_two:
+	addu	 $t9, $t8, $t5 # $t9 = i*ncol + j
+	mul 	 $t9, $t9, sizeF #$t9 = $t9 * 4
+	addu	 $t9, $t9, $t0 
+	
+	#float valor = datos[f * numCol + c];
+	l.s		 $f5, 0($t9) #asigno el valor de ese punto
+	#if (valor < min) {
+	c.lt.s 	$f5, $f4
+	bc1f 	salto_find
+	mov.s 	$f4, $f5 #min = valor;
+	move 	$t6, $t4 #fmin = f;
+	move 	$t7, $t5 #cmin = c;
+
+salto_find:
+	addi 	$t5, $t5, 1
+	blt 	$t5, $t2, for_find_min_two
+
+	addi 	$t4, $t4, 1
+	blt 	$t4, $t1, for_find_min_one
+
+find_out:
+
+	mov.s 	$f0, $f4
+	move 	$v0, $t6
+	move 	$v1, $t7
+
+	jr 		$ra
+
+find_min_fin:
+
+# MAIN
+# $s0 = se almacena la opción elegida
+# $s1 = Se almacena la matriz elegida
+# $s7 = Se almacena la dirección de la matriz elegida
+
+main:
+
+	#std::cout << "\nComienza programa manejo matrices con funciones\n";
+	li 		$v0, 4
+	la 		$a0, str_titulo
+	syscall
+	#structMat* matTrabajo = &mat1;
+	la 		$s7, mat1
+while:
+
+	#print_mat(matTrabajo);
+	move 	$a1, $s7
+	jal	 	print_mat
+
+	# std::cout <<  "(0) Terminar el programa\n"
+	# "(1) Cambiar la matriz de trabajo\n"
+  	# "(3) Cambiar el valor de un elemento\n"
+ 	# "(4) Intercambiar un elemento con su opuesto\n"
+ 	# "(7) Encontrar el minimo\n"
+  	# "\nIntroduce opción elegida: ";
+	li 		$v0, 4
+	la 		$a0, str_menu
+	syscall
+
+	li 		$v0, 5
+	syscall
+
+	move 	$s0, $v0
+	beq		$s0, opc_0, termina_programa
+	beq 	$s0, opc_1, cambiar_matriz
+	beq 	$s0, opc_2, ordenacion_burbuja
+	beq 	$s0, opc_3, cambiar_intercambiar_valor_elemento
+	beq 	$s0, opc_4, cambiar_intercambiar_valor_elemento
+	beq 	$s0, opc_7, encontrar_minimo
+	
+	# std::cout << "Numero de matriz de trabajo incorrecto\n";
+	li 		$v0, 4
+	la 		$a0, str_errorOpc
+	syscall
+	
+	j 		while
+
+while_fin:
+
+# $s1 --> almacenamos la matriz escogida
+cambiar_matriz:
+	# std::cout << "\nElije la matriz de trabajo (1..6): ";
+	li 		$v0, 4
+	la 		$a0, str_elijeMat
+	syscall
+
+	li 		$v0, 5
+	syscall
+	move 	$s1, $v0 
+
+	beq 	$s1, opc_1, cambiar1 # if (matT == 1) {
+	beq 	$s1, opc_2, cambiar2 # if (matT == 2) {
+	beq 	$s1, opc_3, cambiar3 # if (matT == 3) {
+	beq 	$s1, opc_4, cambiar4 # if (matT == 4) {
+	beq 	$s1, opc_5, cambiar5 # if (matT == 5) {
+	beq 	$s1, opc_6, cambiar6 # if (matT == 6) {
+
+	# std::cout << "Numero de matriz de trabajo incorrecto\n";
+	# continue;  // volvemos al principio del bucle
+incorrecto:
+	li 		$v0, 4
+	la 		$a0, str_numMatMal
+	syscall
+	j 		while
+
+	# matTrabajo = &mat1;
+	# continue; 
+cambiar1:
+	la 		$s7, mat1
+	j 		while
+
+	# matTrabajo = &mat2;
+	# continue; 
+cambiar2:
+	la 		$s7, mat2
+	j 		while
+
+	# matTrabajo = &mat3;
+	# continue; 
+cambiar3:
+	la 		$s7, mat3
+	j 		while
+
+	# matTrabajo = &mat4;
+	# continue; 
+cambiar4:
+	la 		$s7, mat4
+	j 		while
+
+	# matTrabajo = &mat5;
+	# continue; 
+cambiar5:
+	la 		$s7, mat5
+	j 		while
+
+	# matTrabajo = &mat6;
+	# continue; 
+cambiar6:
+	la 		$s7, mat6
+	j 		while
+
+
+cambiar_matriz_fin: 
+
+# $a1 --> dirección de la matriz
+# $a2 --> fila del elemento
+# $a3 --> columna del elemento
+# $f12 --> nuevo flotante
+# $s2 --> filas
+# $s3 --> columnas
+# $s4 --> índice fila
+# s5 --> índice columna
+cambiar_intercambiar_valor_elemento:
+	
+	lw $s2, 0($s7)  #nFila
+	lw $s3, 4($s7)	#nColumna
+	
+	#std::cout << "\nIndice de fila: ";
+	li $v0, 4
+	la $a0, str_indFila
+	syscall
+	
+	#std::cin >> indFil;
+	li $v0, 5
+	syscall
+	move $s4, $v0
+	
+	#if ((indFil < 0) || (indFil >= matTrabajo->nFil)) {
+	slt $t0, $s4, $zero
+	sge $t1, $s4, $s2
+	or $t0, $t0, $t1
+	bnez $t0, error_fila
+
+	#std::cout << "Indice de columna: ";
+	li $v0, 4
+	la $a0, str_indCol
+	syscall
+
+	#std::cin >> indCol;
+	li $v0, 5
+	syscall
+	move $s5, $v0
+
+	#if ((indCol < 0) || (indCol >= matTrabajo->nCol)){
+	slt $t0, $s5, $zero
+	sge $t1, $s5, $s3
+	or $t0, $t0, $t1
+	bnez $t0, error_columna
+
+	beq $s0, opc_3, cambiar_valor
+	#si la opción escogida es 4 salta a la subrutina intercambia
+	move $a1, $s7
+	move $a2, $s4
+	move $a3, $s5
+	jal intercambia
+	j while
+
+
+	cambiar_valor:
+	#std::cout << "Nuevo valor para el elemento: ";
+	li $v0, 4
+	la $a0, str_nuevoValor
+	syscall
+
+	#std::cin >> valor;
+	li $v0, 6
+	syscall
+	
+
+	#change_elto(matTrabajo, indFil, indCol, valor);
+	mov.s $f12, $f0 #almaceno el valor en el registro f12 para pasarlo como argumento
+	move $a1, $s7 # lo mismo con la dirección de la matriz y el resto de elementos
+	move $a2, $s4
+	move $a3, $s5
+	jal change_elto
+	j while
+	cambiar_valor_fin:
+	
+
+# std::cerr << "Error: dimension incorrecta.  Numero de fila incorrecto\n";
+error_fila:
+	li 	$v0, 4
+	la $a0, str_errorFil
+	syscall
+	#continue;
+	j while
+#  std::cerr << "Error: dimension incorrecta.  Numero de columna incorrecto\n";
+error_columna:
+	li 	$v0, 4
+	la $a0, str_errorCol
+	syscall
+	#continue;
+	j while
+
+cambiar_intercambiar_valor_elemento_fin:
+
+# $s0 --> índice fila mínimo
+# $s1 --> índice columna mínimo
+# $f20 --> valor flotante mínimo
+encontrar_minimo:
+	move $a1, $s7
+	#std::tie(valorMin, filaMin, columnaMin) = find_min(matTrabajo);
+	jal find_min
+	move $s0, $v0 
+	move $s1, $v1
+	mov.s $f20, $f0
+	
+	#std::cout << "\nEl valor minimo esta en (" << filaMin << ','
+	li $v0, 4
+	la $a0, str_valMin
+	syscall
+
+	li $v0, 1
+	move $a0, $s0
+	syscall
+
+	# << ','
+	li $a0, 44
+	li $v0, 11
+	syscall
+	
+	#<< columnaMin <<") con valor " << valorMin;
+	li $v0, 1
+	move $a0, $s1
+	syscall
+
+	li $v0, 4
+	la $a0, str_conValor
+	syscall
+	
+	mov.s $f12, $f20
+	li $v0, 2
+	syscall
+
+	j while
+encontrar_minimo_fin:
+
+
+# MODIFICACIÓN-PRUEBA
+# $s2 --> filas
+# $s3 --> columnas
+# $s4 --> índice fila
+# $s7 --> dirección de la matriz
+ordenacion_burbuja:
+	
+	lw $s2, 0($s7) #filas
+	#std::cout << "\nIndice de fila: ";
+	li $v0, 4
+	la $a0, str_indFila
+	syscall
+
+	#std::cin >> indFil;
+	li $v0, 5
+	syscall
+	move $s4, $v0
+
+	#if ((indFil < 0) || (indFil >= matTrabajo->nFil)) {
+	slt $t0, $s4, $zero
+	sge $t1, $s4, $s2
+	or $t0, $t0, $t1
+	bnez $t0, error_fila
+
+	move $a0, $s7 #dirección
+	move $a2, $s4 #índice fila
+	
+	jal sort_row
+
+	j while
+ordenacion_burbuja_fin:
+
+
+sort_row:
+		
+	la $t0, 8($a0) #Primer elemento
+	lw $t1, 0($a0) #nFil
+	lw $t2, 4($a0) #nColum
+	
+	addi $t3, $t2, -1 #nCol - 1
+	move $t9, $a2 #índice fila
+
+
+	
+	mul $t8, $t1,$t2
+	beqz $t8, salir_sort_row
+
+	move $t4, $zero # inicializo el índice de filas a 0 (i)
+
+# for(int f = 0; f < nFil; f++) {
+for_sort: 
+	move $t5, $zero # inicializo el índice de columnas a 0 (j)
+	
+
+segundo_for_sort:
+	#hallo la posición del primer elemento
+	mul $t7, $a2, $t2 #nCol * índFila
+	add $t7, $t7, $t5 #nCol * indFila + j
+	mul $t7, $t7, sizeF
+	addu $t7, $t7, $t0
+	l.s $f4, 0($t7)
+
+	#hallo el del segundo
+	mul $t8, $t9, $t2
+	add $t8, $t8, $t5
+	addi $t8, $t8, 1
+	mul $t8, $t8, sizeF
+	addu $t8, $t8, $t0
+	l.s $f5, 0($t8)
+
+	#comparo el valor de los dos
+	c.le.s $f4, $f5
+	bc1t salto_sort # si no se cumple que sea mayor se salta el if
+	move $a0, $t7
+	move $a3, $t8
+	
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal swap
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
 
-	jr $ra
+salto_sort:
+	
+	sub $t6, $t3, $t4 #nCol - 1 - i
+	addi $t5, $t5, 1 # j++
+	blt $t5, $t6, segundo_for_sort
 
-intercambia_fin:
+	addi $t4, $t4, 1 # i++
+	blt $t4, $t3, for_sort
+
+
+salir_sort_row:
+
+	jr $ra
+sort_row_fin:
+
+termina_programa:
+	li $v0, 4
+	la $a0, str_termina
+	syscall
+	
+	li $v0, 10
+	syscall
