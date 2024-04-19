020ecd55b70b1963af4cb92f4340e6da465eb227
Joancgm05 <alu010165571@ull.edu.es>
2024-04-19 11:35:40 +0100

    mc

--- z/a.cc
+++ z/a.cc
@@ -50,3 +50,50 @@ template<class T> int stack_l_t<T>::suma_pares(){
 }
 
 #endif  // STACKL_H_
+
+/**
+   * Modificación
+   * Contar el número de pares en una pila
+  */
+
+  stack_l_t<int> modi; 
+
+  modi.push(8);
+  modi.push(2);
+  modi.push(4);
+  modi.push(3);
+  modi.push(6);
+  modi.push(3);
+  cout << "El numero de pares es : " << modi.cont_pair() << endl;
+
+  /**
+   * Modificación2
+   * calcular la media de una pila
+  */
+
+  stack_l_t<int> modi2;
+
+  modi2.push(8);
+  modi2.push(2);
+  modi2.push(4);
+  modi2.push(3);
+  modi2.push(6);
+  modi2.push(3);
+  cout << "La media de la pila es : " << modi2.media() << endl;
+
+  /** 
+   * Modificación 3
+   * Suma de los pares de una pila
+  */
+  stack_l_t<int> modi3;
+
+  modi3.push(8);
+  modi3.push(2);
+  modi3.push(4);
+  modi3.push(3);
+  modi3.push(6);
+  modi3.push(3);
+  cout << "La suma de los pares de la pila es : " << modi3.suma_pares() << endl;
+
+  return 0;
+}
