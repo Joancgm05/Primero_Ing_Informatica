371c498452f51a1c5d3a12978af6eb7b9f5f6069
Joancgm05 <alu0101656571@ull.edu.es>
2024-04-19 11:22:51 +0100

    mc

--- AyED/Practicas/Practica_5/rpn_t.h
+++ AyED/Practicas/Practica_5/rpn_t.h
@@ -1,12 +1,3 @@
-// AUTOR: Joan Carlos García Méndez
-// FECHA: 19 - 04 - 2024
-// EMAIL: alu 0101656571@ull.edu.es
-// VERSION: 2.0
-// ASIGNATURA: Algoritmos y Estructuras de Datos
-// PRÁCTICA Nº: 5
-// ESTILO: Google C++ Style Guide
-// COMENTARIOS: Clase RPN (Reverse Polish Notation)
-
 #ifndef RPNT_H_
 #define RPNT_H_
 
@@ -18,8 +9,7 @@
 #include "queue_l_t.h"
 
 // Clase RPN (Reverse Polish Notation)
-template <class T> 
-class rpn_t {
+template <class T> class rpn_t {
  public:
   // constructor
  rpn_t(void) : stack_() {}
@@ -28,11 +18,12 @@ class rpn_t {
   ~rpn_t() {}
 
   // operaciones
-  const int evaluate(queue_l_t<char>&); // evalúa una expresión en notación polaca inversa
+  const int evaluate(queue_l_t<char>&);
 
+  
  private: 
-  T stack_; // pila de la calculadora
-  void operate_(const char operador); // realiza una operación
+  T stack_;
+  void operate_(const char operador);
 };
 
 
@@ -46,118 +37,81 @@ template<class T> const int rpn_t<T>::evaluate(queue_l_t<char>& q) {
 
     if (isdigit(c)) {
       int i = c - '0';
-      // poner código
+      //poner código
       stack_.push(i);
       std::cout << " (es un dígito) " << std::endl
 		<< "   Lo metemos en la pila..." << std::endl;
     } else {
       std::cout << " (es un operador)" << std::endl;
       // poner código
-      operate_(c); // operamos
+      operate_(c);
     }
   }
   // poner código
-  return stack_.top(); // devolvemos el resultado
+  int resultado = stack_.top();
+  stack_.pop();
+  return (resultado);
 }
 
-template <class T>
-void rpn_t<T>::operate_(const char c) {  // int operand1, int operand2
-  assert(c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == 'r' || // + suma, - resta, * multiplicación, / división, ^ potencia, r raíz cuadrada
-         c == 'c' || c == 'l' || c == '%' || c == '<' || c == '>' || c == 'n' || // c cuadrado, l logaritmo base 2, % módulo, < menor que, > mayor que, n negativo
-         c == 'L' || c == 'C' || c == 'S' || c == 'T' || c == 'b' || c == 'h' ); // L ln, C coseno, S seno, T tangente, b raiz cubica, h raíz cuadrada de la suma de los cuadrados de dos nº
-  // poner código
-  double result;
-  double second_term = stack_.top();  // extraemos el top de la pila
-  stack_.pop();                       // volvemos a extraer
+template<class T> void rpn_t<T>::operate_(const char c) {
+  assert(c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == 'r' || c == 'l' || c == 'c');
 
-  std::cout << "   Sacamos de la pila un operando: " << second_term << std::endl;
-  if (c == 'r' || c == 'l' || c == 'c' || c == 'n' || c == 'L' || c == 'C' || 
-      c == 'S' || c == 'T' || c == 'b'){
-    switch (c) {
-      case 'r':
-        result = sqrt(second_term);
-        break;
-      case 'c':
-        result = pow(second_term, 2);
-        break;
-      case 'l':
-        result = log2(second_term);
-        break;
-      case 'n':
-        result = -second_term;
-        break;
-      case 'L':
-        result = log(second_term);
-        break;
-      case 'C':
-        result = cos(second_term);
-        break;
-      case 'S':
-        result = sin(second_term);
-        break;
-      case 'T':
-        result = tan(second_term);
-        break;
-      case 'b':
-        result = cbrt(second_term);
-        break;
-      default:
-        std::cout << "No se insertó ningún carácter válido para operar" << std::endl;
-        exit(EXIT_SUCCESS);
-    }
-    stack_.push(result);  // devolvemos el resultado
-    std::cout << "Metemos en la pila el resultado: " << result << std::endl;
-  }
-  // poner código
-  else {
-    int top_term = stack_.top();
+  
+  int resultado;
+  //aridad 1
+  if (c == 'r' || c == 'l' || c == 'c') {
+    int num = stack_.top();
     stack_.pop();
-    std::cout << "Sacamos de la pila otro operando: " << top_term << std::endl;
-
-    switch (c) {
-      case '+':
-        // poner código
-        result = top_term + second_term;
+    std::cout << "   Sacamos de la pila un operando: " << num << std::endl;
+    switch(c) {
+      case 'r':
+        resultado = sqrt(num);
         break;
-        // poner código resto de operadores
-      case '-':
-        result = top_term - second_term;
+      
+      case 'l':
+        resultado = log2(num);
         break;
-      case '*':
-        result = top_term * second_term;
-        break;
-      case '/':
-        result = top_term / second_term;
-        break;
-      case '^':
-        result = pow(top_term, second_term);
-        break;
-      case '%':
-        result = (top_term / 100) * second_term;
-        break;
-      case '<':
-        if (top_term < second_term) {
-          result = top_term;
-        } else {
-          result = second_term;
-        }
-        break;
-      case '>':
-        if (top_term > second_term) {
-          result = top_term;
-        } else {
-          result = second_term;
-        }
-        break;
-      case 'h':
-        result = hypot(top_term, second_term);
+      
+      case 'c':
+        resultado = num * num;
         break;
     }
-
+  } else {
     // poner código
-    stack_.push(result);  // devolvemos el resultado
-    std::cout << "Metemos en la pila el resultado: " << result << std::endl;
+    int num2 = stack_.top();
+    stack_.pop();
+    std::cout << "   Sacamos de la pila un operando: " << num2 << std::endl;
+    // poner código
+    int num1 = stack_.top();
+    stack_.pop();
+    std::cout << "   Sacamos de la pila otro operando: " << num1 << std::endl;
+    
+    switch (c) {
+    case '+':
+      resultado = num1 + num2;
+      // poner código
+      break;
+    case '-':
+      resultado = num1 - num2;
+      break;
+    // poner código resto de operadores
+    case '/':
+      resultado = num1 / num2;
+      break;
+    
+    case '*':
+      resultado = num1 * num2;
+      break;
+    
+    case '^':
+      resultado = pow(num1, num2);
+      break;
+    }
+
   }
+  // poner código
+  stack_.push(resultado);
+  std::cout << "   Metemos en la pila el resultado: " << resultado << std::endl;
 }
 
  
