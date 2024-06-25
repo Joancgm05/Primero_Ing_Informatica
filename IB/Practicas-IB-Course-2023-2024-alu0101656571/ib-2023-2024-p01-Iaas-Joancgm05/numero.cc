e5c8cef8b1b3e1211367fca790f8ce48dd5230e7
Joancgm05 <alu0101656571@ull.edu.es>
2024-06-26 00:24:09 +0100

    mc

--- IB/Practicas-IB-Course-2023-2024-alu0101656571/ib-2023-2024-p01-Iaas-Joancgm05/numero.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-  int some_number;
-  std::cin >> some_number;
-  std::cout << " number = " << some_number << std::endl;
-  std::cerr << " boring error message " << std::endl;
-  return 0;
-}
-
